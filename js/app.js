9eb41849ef7601a2e3428a4997be9be0eb20f601
mechee55 <mechee55@gmail.com>
2018-06-26 19:46:38 -0700

    done

---
+++ js/app.js
@@ -0,0 +1,42 @@
+const celInput = document.querySelector('#celcius > input')
+const fahInput = document.querySelector('#fahrenheit > input')
+const kelInput = document.querySelector('#kelvin > input')
+
+function roundNum (num) {
+  return Math.round(num * 100) / 100
+}
+
+function celToFahAndKel () {
+  // without parseFloat, JS will concatenate cTemp AS A STRING when you log kTemp
+  const cTemp = parseFloat(celInput.value)
+  const fTemp = (cTemp * (9 / 5)) + 32
+  const kTemp = cTemp + 273.15
+
+  fahInput.value = roundNum(fTemp)
+  kelInput.value = roundNum(kTemp)
+}
+
+function fahToCelAndKel () {
+  const fTemp = parseFloat(fahInput.value)
+  const cTemp = (fTemp - 32) * (5 / 9)
+  const kTemp = (fTemp + 459.67) * 5 / 9
+
+  celInput.value = roundNum(cTemp)
+  kelInput.value = roundNum(kTemp)
+}
+
+function kelToCelAndFah () {
+  const kTemp = parseFloat(kelInput.value)
+  const cTemp = kTemp - 273.15
+  const fTemp = 9 / 5 * (kTemp - 273) + 32
+  celInput.value = roundNum(cTemp)
+  fahInput.value = roundNum(fTemp)
+}
+
+function main () {
+  celInput.addEventListener('input', celToFahAndKel)
+  fahInput.addEventListener('input', fahToCelAndKel)
+  kelInput.addEventListener('input', kelToCelAndFah)
+}
+
+main()
